8987eb6b30469a366bc0968d7828e64579b907d2
AlexDeLaCreme <lyosha@gmail.com>
2012-06-16 13:16:31 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Djcharts::Application.config.secret_token = '6694a7429097c09b5ae94980094b6002549aa22a309347939fbb74aaec31136d87f278bf299630357d8322ab0a7ef753a2978a668d0a2d59671f08d14cdc58d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Djcharts::Application.config.session_store :cookie_store, key: '_djcharts_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Djcharts::Application.config.session_store :active_record_store
